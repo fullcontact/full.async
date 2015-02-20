b93221adafaba9c45d9d6e5e219ea7cd4ef68b95
Karlis Lauva <skazhy@gmail.com>
2015-02-20 12:18:16 +0200

    Update project url

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
-  :url "https://github.com/fullcontact/full-async"
+  :url "https://github.com/fullcontact/full.async"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
   :plugins [[lein-midje "3.1.3"]]
