6f954e03e304352af834abf4bd42320dc4662b21
Kaspars Dancis <kaspars@fullcontact.com>
2015-03-18 15:25:20 -0600

    lein-release plugin: bumped version from 0.2.3 to 0.2.4-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.async "0.2.3"
+(defproject fullcontact/full.async "0.2.4-SNAPSHOT"
   :description "Extensions and helpers for core.async."
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
