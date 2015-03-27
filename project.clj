3089cee08bf31d7e760b761dc3a0aa8d5f55f487
Kaspars Dancis <kaspars@fullcontact.com>
2015-03-27 10:46:28 -0600

    lein-release plugin: bumped version from 0.1.2 to 0.1.3-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.dev "0.1.2"
+(defproject fullcontact/full.dev "0.1.3-SNAPSHOT"
   :description "Clojure's development and debugging helpers"
   :url "https://github.com/fullcontact/full.dev"
   :license {:name "Eclipse Public License - v 1.0"
