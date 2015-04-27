5bc8481df6b68cd451112ba51de50c82bd1fc1c9
Kaspars Dancis <kaspars@fullcontact.com>
2015-04-27 14:38:46 -0600

    AOT for faster load times

--- project.clj
+++ project.clj
@@ -5,13 +5,15 @@
 
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
-            :distribution :repo}  
+            :distribution :repo}
 
-  :deploy-repositories [["releases" {:url "https://clojars.org/repo/" :creds :gpg}]]                   
+  :deploy-repositories [["releases" {:url "https://clojars.org/repo/" :creds :gpg}]]
 
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
 
-  :plugins [[lein-midje "3.1.3"]]  
+  :aot :all
+
+  :plugins [[lein-midje "3.1.3"]]
 
   :profiles {:dev {:dependencies [[midje "1.6.3"]]}})
