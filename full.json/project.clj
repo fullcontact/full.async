2e511645e8d9b9f373a7a7a3de132f59889f681f
Zach Arlen <nelrahcaz@gmail.com>
2015-04-30 13:03:39 -0600

    Version 0.4.10

--- full.json/project.clj
+++ full.json/project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.json "0.4.10-SNAPSHOT"
+(defproject fullcontact/full.json "0.4.10"
   :description "Read and write JSON (Cheshire extension)."
 
   :url "https://github.com/fullcontact/full.monty"
@@ -11,9 +11,9 @@
 
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [cheshire "5.3.1"]
-                 [fullcontact/full.time "0.4.10-SNAPSHOT"]
-                 [fullcontact/camelsnake "0.4.10-SNAPSHOT"]
-                 [fullcontact/full.core "0.4.10-SNAPSHOT"]]
+                 [fullcontact/full.time "0.4.10"]
+                 [fullcontact/camelsnake "0.4.10"]
+                 [fullcontact/full.core "0.4.10"]]
 
   :aot :all
 
