58990fec32f966028ed37667588211d3ce57cd0c
Kaspars Dancis <kaspars@fullcontact.com>
2015-05-07 13:56:21 -0600

    Version 0.4.12

--- full.core/project.clj
+++ full.core/project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.core "0.4.12-SNAPSHOT"
+(defproject fullcontact/full.core "0.4.12"
   :description "FullContact's core Clojure library - logging, configuration and common helpers."
 
   :url "https://github.com/fullcontact/full.monty"
