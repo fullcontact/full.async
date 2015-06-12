56473d249014bd5ea28623c77bc1dbc906d9f0c3
Kaspars Dancis <kaspars@fullcontact.com>
2015-06-12 15:48:16 -0600

    Version 0.4.21-SNAPSHOT

--- camelsnake/project.clj
+++ camelsnake/project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/camelsnake "0.4.20"
+(defproject fullcontact/camelsnake "0.4.21-SNAPSHOT"
   :description "String and keyword transformation between cases."
 
   :url "https://github.com/fullcontact/full.monty"
