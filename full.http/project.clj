2cce0a26096a3f67175c2d2aebf3dd8561513fc2
Kaspars Dancis <kaspars@fullcontact.com>
2015-06-12 17:37:38 -0600

    0.4.22-SNAPSHOT

--- full.http/project.clj
+++ full.http/project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.http "0.4.21"
+(defproject fullcontact/full.http "0.4.22-SNAPSHOT"
   :description "Async HTTP client and server on top of http-kit and core.async."
 
   :url "https://github.com/fullcontact/full.monty"
@@ -14,11 +14,11 @@
                  [compojure "1.3.1"]
                  [javax.servlet/servlet-api "2.5"]
                  [ring-cors "0.1.7"]
-                 [fullcontact/camelsnake "0.4.21"]
-                 [fullcontact/full.json "0.4.21"]
-                 [fullcontact/full.metrics "0.4.21"]
-                 [fullcontact/full.async "0.4.21"]
-                 [fullcontact/full.core "0.4.21"]]
+                 [fullcontact/camelsnake "0.4.22-SNAPSHOT"]
+                 [fullcontact/full.json "0.4.22-SNAPSHOT"]
+                 [fullcontact/full.metrics "0.4.22-SNAPSHOT"]
+                 [fullcontact/full.async "0.4.22-SNAPSHOT"]
+                 [fullcontact/full.core "0.4.22-SNAPSHOT"]]
 
   :aot :all
 
--- full.json/project.clj
+++ full.json/project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.json "0.4.21"
+(defproject fullcontact/full.json "0.4.22-SNAPSHOT"
   :description "Read and write JSON (Cheshire extension)."
 
   :url "https://github.com/fullcontact/full.monty"
@@ -11,9 +11,9 @@
 
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [cheshire "5.3.1"]
-                 [fullcontact/full.time "0.4.21"]
-                 [fullcontact/camelsnake "0.4.21"]
-                 [fullcontact/full.core "0.4.21"]]
+                 [fullcontact/full.time "0.4.22-SNAPSHOT"]
+                 [fullcontact/camelsnake "0.4.22-SNAPSHOT"]
+                 [fullcontact/full.core "0.4.22-SNAPSHOT"]]
 
   :aot :all
 
