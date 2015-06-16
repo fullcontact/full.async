dc3bd1ea10b8624b195134701685b244a3bb83b0
Kaspars Dancis <kaspars@fullcontact.com>
2015-06-16 08:54:52 -0600

    full.async - return vector from <?* amd <!*

--- full.async/src/full/async.clj
+++ full.async/src/full/async.clj
@@ -101,7 +101,7 @@
          (->> (<! head#)
               (conj results#)
               (recur (rest chs#)))
-         (seq results#)))))
+         (vec results#)))))
 
 (defmacro <?*
   "Takes one result from each channel and returns them as a collection.
@@ -115,7 +115,7 @@
          (->> (<? head#)
               (conj results#)
               (recur (rest chs#)))
-         (seq results#)))))
+         (vec results#)))))
 
 (defn <!!*
   [chs]
