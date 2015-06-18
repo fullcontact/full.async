9c07bd4afba8fe6ec553f780500fc1ee37e38a78
Kaspars Dancis <kaspars@fullcontact.com>
2015-06-18 17:25:58 -0600

    Version 0.5.1

--- full.http/project.clj
+++ full.http/project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.http "0.5.1-SNAPSHOT"
+(defproject fullcontact/full.http "0.5.1"
   :description "Async HTTP client and server on top of http-kit and core.async."
 
   :url "https://github.com/fullcontact/full.monty"
@@ -14,11 +14,11 @@
                  [compojure "1.3.1"]
                  [javax.servlet/servlet-api "2.5"]
                  [ring-cors "0.1.7"]
-                 [fullcontact/camelsnake "0.5.1-SNAPSHOT"]
-                 [fullcontact/full.json "0.5.1-SNAPSHOT"]
-                 [fullcontact/full.metrics "0.5.1-SNAPSHOT"]
-                 [fullcontact/full.async "0.5.1-SNAPSHOT"]
-                 [fullcontact/full.core "0.5.1-SNAPSHOT"]]
+                 [fullcontact/camelsnake "0.5.1"]
+                 [fullcontact/full.json "0.5.1"]
+                 [fullcontact/full.metrics "0.5.1"]
+                 [fullcontact/full.async "0.5.1"]
+                 [fullcontact/full.core "0.5.1"]]
 
   :aot :all
 
