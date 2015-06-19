92c82dbcb7b588eb5773882bc94fe91ee163f549
Kaspars Dancis <kaspars@fullcontact.com>
2015-06-19 16:52:29 -0600

    full.http.server - wrap-cookies> middleware

--- full.http/src/full/http/server.clj
+++ full.http/src/full/http/server.clj
@@ -10,6 +10,8 @@
             [ring.middleware.params :refer [wrap-params]]
             [ring.middleware.resource :refer [resource-request]]
             [ring.middleware.content-type :refer [content-type-response]]
+            [ring.util.codec :as codec]
+            [ring.middleware.cookies :refer [cookies-request cookies-response]]
             [org.httpkit.server :as httpkit]
             [full.core.sugar :refer :all]
             [full.core.log :as log]
@@ -205,6 +207,47 @@
     (go-try
       (if-let [resp (<? (handler> req))]
         (content-type-response resp req opts)))))
+
+(defn wrap-cookies>
+  "Parses the cookies in the request map, then assocs the resulting map
+       to the :cookies key on the request.
+
+       Accepts the following options:
+
+       :decoder - a function to decode the cookie value. Expects a function that
+                  takes a string and returns a string. Defaults to URL-decoding.
+
+       :encoder - a function to encode the cookie name and value. Expects a
+                  function that takes a name/value map and returns a string.
+                  Defaults to URL-encoding.
+
+       Each cookie is represented as a map, with its value being held in the
+       :value key. A cookie may optionally contain a :path, :domain or :port
+       attribute.
+
+       To set cookies, add a map to the :cookies key on the response. The values
+       of the cookie map can either be strings, or maps containing the following
+       keys:
+
+       :value     - the new value of the cookie
+       :path      - the subpath the cookie is valid for
+       :domain    - the domain the cookie is valid for
+       :max-age   - the maximum age in seconds of the cookie
+       :expires   - a date string at which the cookie will expire
+       :secure    - set to true if the cookie requires HTTPS, prevent HTTP access
+       :http-only - set to true if the cookie is valid for HTTP and HTTPS only
+                    (ie. prevent JavaScript access)"
+  {:arglists '([handler] [handler options])}
+  [handler> & [{:keys [decoder encoder]
+                :or {decoder codec/form-decode-str
+                     encoder codec/form-encode}}]]
+  (fn [req]
+    (go-try
+      (-> req
+          (cookies-request {:decoder decoder})
+          (handler>)
+          <?
+          (cookies-response {:encoder encoder})))))
 
 (defn- send-async
   "Private middleware that sends the response asynchronously via http-kit's
