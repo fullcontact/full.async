e1615fec948a705338c55408995a41ea78fd8305
Kaspars Dancis <kaspars@fullcontact.com>
2015-06-18 18:00:13 -0600

    Version 0.5.3-SNAPSHOT

--- full.rollbar/project.clj
+++ full.rollbar/project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.rollbar "0.5.2"
+(defproject fullcontact/full.rollbar "0.5.3-SNAPSHOT"
   :description "Library to ship exceptions and request information to the rollbar logging service."
 
   :url "https://github.com/fullcontact/full.monty"
@@ -10,10 +10,10 @@
   :deploy-repositories [["releases" {:url "https://clojars.org/repo/" :creds :gpg}]]
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [fullcontact/camelsnake "0.5.2"]
-                 [fullcontact/full.async "0.5.2"]
-                 [fullcontact/full.core "0.5.2"]
-                 [fullcontact/full.http "0.5.2"]]
+                 [fullcontact/camelsnake "0.5.3-SNAPSHOT"]
+                 [fullcontact/full.async "0.5.3-SNAPSHOT"]
+                 [fullcontact/full.core "0.5.3-SNAPSHOT"]
+                 [fullcontact/full.http "0.5.3-SNAPSHOT"]]
   :aot :all
 
   :plugins [[lein-midje "3.1.3"]]
