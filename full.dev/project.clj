8f15dc9261f3d123f801afa32504200e2606e050
Kaspars Dancis <kaspars@fullcontact.com>
2015-06-19 14:09:01 -0600

    Version 0.5.4-SNAPSHOT

--- full.dev/project.clj
+++ full.dev/project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.dev "0.5.3"
+(defproject fullcontact/full.dev "0.5.4-SNAPSHOT"
   :description "Clojure's development and debugging helpers"
 
   :url "https://github.com/fullcontact/full.monty"
@@ -11,7 +11,7 @@
 
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [ns-tracker "0.2.2"]
-                 [fullcontact/full.core "0.5.3"]]
+                 [fullcontact/full.core "0.5.4-SNAPSHOT"]]
 
   :aot :all
 
