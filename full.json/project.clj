351b48d1c8b9315b8c33a020dfb2e3d3cb1cf30a
Kaspars Dancis <kaspars@fullcontact.com>
2015-06-19 14:04:50 -0600

    Version 0.5.3

--- full.json/project.clj
+++ full.json/project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.json "0.5.3-SNAPSHOT"
+(defproject fullcontact/full.json "0.5.3"
   :description "Read and write JSON (Cheshire extension)."
 
   :url "https://github.com/fullcontact/full.monty"
@@ -11,9 +11,9 @@
 
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [cheshire "5.5.0"]
-                 [fullcontact/full.time "0.5.3-SNAPSHOT"]
-                 [fullcontact/camelsnake "0.5.3-SNAPSHOT"]
-                 [fullcontact/full.core "0.5.3-SNAPSHOT"]]
+                 [fullcontact/full.time "0.5.3"]
+                 [fullcontact/camelsnake "0.5.3"]
+                 [fullcontact/full.core "0.5.3"]]
 
   :aot :all
 
