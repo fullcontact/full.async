9a05f893d35e25187226f99ebf7730892bce47c2
Kaspars Dancis <kaspars@fullcontact.com>
2015-07-02 12:47:07 -0600

    Version 0.5.10-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.async "0.5.9"
+(defproject fullcontact/full.async "0.5.10-SNAPSHOT"
   :description "Extensions and helpers for core.async."
 
   :url "https://github.com/fullcontact/full.monty"
