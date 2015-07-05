791b090834b82fdb8f017d0004983f46652aa1c7
Christian Weilbach <christian_weilbach@web.de>
2015-07-05 14:30:57 +0200

    Add loop macros and go-for comprehension.
    Extend some docs and add notes.

--- src/full/async.clj
+++ src/full/async.clj
@@ -1,6 +1,7 @@
 (ns full.async
   (:require [clojure.core.async :refer [<! <!! >! alts! go go-loop chan] :as async]))
 
+
 (defn throw-if-throwable
   "Helper method that checks if x is Throwable and if yes, wraps it in a new
   exception, passing though ex-data if any, and throws it. The wrapping is done
@@ -38,6 +39,11 @@
   `(go (try ~@body (catch Throwable e# e#))))
 
 (defmacro go-retry
+  "Asynchronously executes the body in a go block. Returns a channel
+  which will receive the result of the body when completed. In case of
+  an exception (type might be given) repeats retries times with a
+  delay. An error function can be provided to cover errors
+  separately. Returns the throwable if the retries fail."
   [{:keys [exception retries delay error-fn]
     :or {error-fn nil, exception Throwable, retries 5, delay 1}} & body]
   `(let [error-fn# ~error-fn]
@@ -52,22 +58,46 @@
              (recur (dec retries#)))
            res#)))))
 
+(defmacro go-try> [err-chan & body]
+  "Same as go-try, but puts errors directly on a channel and returns
+  nil on the resulting channel."
+  `(go (try
+         ~@body
+         (catch Throwable e#
+           (>! ~err-chan e#)))))
+
+(defmacro go-loop-try> [err-chan bindings & body]
+  "Put throwables arising in the go-loop on an error channel."
+  `(go (try
+         (loop ~bindings
+           ~@body)
+         (catch Throwable e#
+           (>! ~err-chan e#)))))
+
+(defmacro go-loop-try [bindings & body]
+  "Returns result of the loop or a throwable in case of an exception."
+  `(go-try (loop ~bindings ~@body) ))
+
 (defmacro <<!
   "Takes multiple results from a channel and returns them as a vector.
-  The input channel must be closed."
+  Waits for input channel to be closed."
   [ch]
-  `(let [ch# ~ch]
+  `(let [ch# ~ch] ;; we only need this when non-gensym symbols are used in macro bindings (unhygienic).
+     ;; otherwise ch can and should never be shadowed, right?
      (<! (async/into [] ch#))))
+
 
 (defmacro <<?
   "Takes multiple results from a channel and returns them as a vector.
-  Throws if any result is an exception."
+  Throws if any result is an exception. Waits for input channel to be
+  closed."
   [ch]
   `(->> (<<! ~ch)
         (map throw-if-throwable)
         ; doall to check for throwables right away
         (doall)))
 
+;; TODO lazy-seq vs. vector in <<! ?
 (defn <<!!
   [ch]
   (lazy-seq
@@ -117,12 +147,16 @@
   [chs]
   (<?? (go-try (<?* chs))))
 
+;; transducer for parallelism?
 (defn pmap>>
-  "Takes objects from ch, asynchrously applies function f> (function should
-  return channel), takes the result from the returned channel and if it's not
-  nil, puts it in the results channel. Returns the results channel. Closes the
-  returned channel when the input channel has been completely consumed and all
-  objects have been processed."
+  "Takes objects from ch, asynchrously applies function f> (function
+  should return channel), takes the result from the returned channel
+  and if it's not nil, puts it in the results channel. Returns the
+  results channel. Closes the returned channel when the input channel
+  has been completely consumed and all objects have been processed. f>
+  should not do io-heavy jobs as this can dry out the core.async
+  thread pool for high degrees of parallelism, use async/thread to
+  wrap such tasks."
   [f> parallelism ch]
   (let [results (async/chan)
         threads (atom parallelism)]
@@ -158,3 +192,76 @@
           (>! result acc)
           (async/close! result))))
     result))
+
+
+;; taken from clojure/core ~ 1.7
+(defmacro ^{:private true} assert-args
+  [& pairs]
+  `(do (when-not ~(first pairs)
+         (throw (IllegalArgumentException.
+                 (str (first ~'&form) " requires " ~(second pairs) " in " ~'*ns* ":" (:line (meta ~'&form))))))
+       ~(let [more (nnext pairs)]
+          (when more
+            (list* `assert-args more)))))
+
+
+(defmacro go-for
+  "List comprehension adapted from clojure.core 1.7. Takes a vector of
+  one or more binding-form/collection-expr pairs, each followed by
+  zero or more modifiers, and yields a channel of evaluations of
+  expr. It is eager on all but the outer-most collection. TODO
+
+  Collections are iterated in a nested fashion, rightmost fastest, and
+  nested coll-exprs can refer to bindings created in prior
+  binding-forms.  Supported modifiers are: :let [binding-form expr
+  ...],
+   :while test, :when test.
+
+  (<! (async/into [] (go-for [x (range 10) :let [y (<! (go 4))] :while (< x y)] [x y])))"
+  {:added "1.0"}
+  [seq-exprs body-expr]
+  (assert-args
+   (vector? seq-exprs) "a vector for its binding"
+   (even? (count seq-exprs)) "an even number of forms in binding vector")
+  (let [to-groups (fn [seq-exprs]
+                    (reduce (fn [groups [k v]]
+                              (if (keyword? k)
+                                (conj (pop groups) (conj (peek groups) [k v]))
+                                (conj groups [k v])))
+                            [] (partition 2 seq-exprs)))
+        err (fn [& msg] (throw (IllegalArgumentException. ^String (apply str msg))))
+        emit-bind (fn emit-bind [res-ch [[bind expr & mod-pairs]
+                                        & [[_ next-expr] :as next-groups]]]
+                    (let [giter (gensym "iter__")
+                          gxs (gensym "s__")
+                          do-mod (fn do-mod [[[k v :as pair] & etc]]
+                                   (cond
+                                     (= k :let) `(let ~v ~(do-mod etc))
+                                     (= k :while) `(when ~v ~(do-mod etc))
+                                     (= k :when) `(if ~v
+                                                    ~(do-mod etc)
+                                                    (recur (rest ~gxs)))
+                                     (keyword? k) (err "Invalid 'for' keyword " k)
+                                     next-groups
+                                     `(let [iterys# ~(emit-bind res-ch next-groups)
+                                            fs# (<? (iterys# ~next-expr))]
+                                        (if fs#
+                                          (concat fs# (<? (~giter (rest ~gxs))))
+                                          (recur (rest ~gxs))))
+                                     :else `(do (>! ~res-ch ~body-expr)
+                                                (<? (~giter (rest ~gxs))))
+                                     #_`(cons ~body-expr (<? (~giter (rest ~gxs))))))]
+                      `(fn ~giter [~gxs]
+                         (go-try
+                          (loop [~gxs ~gxs]
+                            (let [~gxs (seq ~gxs)]
+                              (when-let [~bind (first ~gxs)]
+                                ~(do-mod mod-pairs))))))))
+        res-ch (gensym "res_ch__")]
+    `(let [~res-ch (chan)
+           iter# ~(emit-bind res-ch (to-groups seq-exprs))]
+       (go (try (<? (iter# ~(second seq-exprs)))
+                (catch Throwable e#
+                  (>! ~res-ch e#))
+                (finally (async/close! ~res-ch))))
+       ~res-ch)))
--- test/full/t_async.clj
+++ test/full/t_async.clj
@@ -1,78 +1,132 @@
 (ns full.t-async
   (:require [midje.sweet :refer :all]
             [full.async :refer :all]
-            [clojure.core.async :refer [<!! >! >!! go chan close!]]))
+            [clojure.core.async :refer [<!! <! >! >!! go chan close! onto-chan]]))
 
 (facts
-  (fact
-    (<!! (go (let [ch (chan 2)]
-               (>! ch "1")
-               (>! ch "2")
-               (close! ch)
-               (<<! ch))))
-    => ["1" "2"])
+ (fact
+  (<!! (go (let [ch (chan 2)]
+             (>! ch "1")
+             (>! ch "2")
+             (close! ch)
+             (<<! ch))))
+  => ["1" "2"])
 
-  (fact
-    (<!! (go (let [ch (chan 2)]
-               (>! ch "1")
-               (>! ch "2")
-               (close! ch)
-               (<<! ch))))
-    => ["1" "2"])
+ (fact
+  (<!! (go (let [ch (chan 2)]
+             (>! ch "1")
+             (>! ch "2")
+             (close! ch)
+             (<<! ch))))
+  => ["1" "2"])
 
-  (fact
-    (<?? (go-try (let [ch (chan 2)]
-               (>! ch "1")
-               (>! ch (Exception.))
-               (close! ch)
-               (<<? ch))))
-    => (throws Exception))
+ (fact
+  (<?? (go-try (let [ch (chan 2)]
+                 (>! ch "1")
+                 (>! ch (Exception.))
+                 (close! ch)
+                 (<<? ch))))
+  => (throws Exception))
 
-  (fact
-    (<<!! (let [ch (chan 2)]
-            (>!! ch "1")
-            (>!! ch "2")
-            (close! ch)
-            ch))
-    => ["1" "2"])
+ (fact
+  (<<!! (let [ch (chan 2)]
+          (>!! ch "1")
+          (>!! ch "2")
+          (close! ch)
+          ch))
+  => ["1" "2"])
 
-  (fact
-    (<!! (go (<<! (let [ch (chan 2)]
-                    (>! ch "1")
-                    (>! ch "2")
-                    (close! ch)
-                    ch))))
-    => ["1" "2"])
+ (fact
+  (<!! (go (<<! (let [ch (chan 2)]
+                  (>! ch "1")
+                  (>! ch "2")
+                  (close! ch)
+                  ch))))
+  => ["1" "2"])
 
-  (fact
-    (<<?? (let [ch (chan 2)]
-            (>!! ch "1")
-            (>!! ch (Exception.))
-            (close! ch)
-            ch))
-    => (throws Exception))
+ (fact
+  (<<?? (let [ch (chan 2)]
+          (>!! ch "1")
+          (>!! ch (Exception.))
+          (close! ch)
+          ch))
+  => (throws Exception))
 
-  (fact
-    (<!!* [(go "1") (go "2")])
-    => ["1" "2"])
+ (fact
+  (<!!* [(go "1") (go "2")])
+  => ["1" "2"])
 
-  (fact
-    (<??* [(go "1") (go "2")])
-    => ["1" "2"])
+ (fact
+  (<??* [(go "1") (go "2")])
+  => ["1" "2"])
 
-  (fact
-    (<??* (list (go "1") (go "2")))
-    => ["1" "2"])
+ (fact
+  (<??* (list (go "1") (go "2")))
+  => ["1" "2"])
 
-  (fact
-    (<??* [(go "1") (go (Exception. ))])
-    => (throws Exception))
+ (fact
+  (<??* [(go "1") (go (Exception. ))])
+  => (throws Exception))
 
-  (fact
-    (->> (let [ch (chan)]
-           (go (doto ch (>!! 1) (>!! 2) close!))
-           ch)
-         (pmap>> #(go (inc %)) 2)
-         (<<??)
-         (set))
-    => #{2 3}))
+ (fact
+  (->> (let [ch (chan)]
+         (go (doto ch (>!! 1) (>!! 2) close!))
+         ch)
+       (pmap>> #(go (inc %)) 2)
+       (<<??)
+       (set))
+  => #{2 3}))
+
+
+;; go-loop-try
+(fact
+ (<<?? (go-loop-try [[f & r] [1 0]]
+                    (/ 1 f)
+                    (recur r)))
+ => (throws Exception))
+
+;; go-try>
+(fact
+ (let [err-ch (chan)]
+   (go-try> err-ch
+            (/ 1 0))
+   (<?? err-ch)
+   => (throws Exception)))
+
+;; go-loop-try>
+(fact
+ (let [err-ch (chan)]
+   (<<?? (go-loop-try> err-ch
+                       [[f & r] [1 0]]
+                       (/ 1 f)
+                       (recur r)))
+   (<?? err-ch)
+   => (throws Exception)))
+
+;; go-for
+(fact ;; traditional for comprehension
+ (<<?? (go-for [a (range 5)
+                :let [c 42]
+                b [5 6]
+                :when (even? b)]
+               [a b c]))
+ => '([0 6 42] [1 6 42] [2 6 42] [3 6 42] [4 6 42]))
+
+
+(fact ;; async operations spliced in bindings and body
+ (<<?? (go-for [a [1 2 3]
+                :let [b (<! (go (* a 2)))]]
+               (<! (go [a b]))))
+ => '([1 2] [2 4] [3 6]))
+
+
+(facts ;; async operations propagate exceptions
+ (<<?? (go-for [a [1 2 3]
+                :let [b 0]]
+               (/ a b)))
+ => (throws Exception)
+
+ (<<?? (go-for [a [1 2 3]
+                :let [b (/ 1 0)]]
+               42))
+ => (throws Exception))
