9b533f3c1909304184bf7d61ff1376a60848e9d4
Kaspars Dancis <kaspars@fullcontact.com>
2015-08-03 09:27:31 -0600

    Version 0.7.4-SNAPSHOT

--- full.bootstrap/project.clj
+++ full.bootstrap/project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.bootstrap "0.7.3"
+(defproject fullcontact/full.bootstrap "0.7.4-SNAPSHOT"
   :description "Boostrap module that pulls in all commonly used full-monty dependencies."
 
   :url "https://github.com/fullcontact/full.monty"
@@ -10,15 +10,15 @@
   :deploy-repositories [["releases" {:url "https://clojars.org/repo/" :creds :gpg}]]
 
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [fullcontact/full.core "0.7.3"]
-                 [fullcontact/full.time "0.7.3"]
-                 [fullcontact/camelsnake "0.7.3"]
-                 [fullcontact/full.json "0.7.3"]
-                 [fullcontact/full.async "0.7.3"]
-                 [fullcontact/full.dev "0.7.3"]
-                 [fullcontact/full.cache "0.7.3"]
-                 [fullcontact/full.metrics "0.7.3"]
-                 [fullcontact/full.http "0.7.3"]]
+                 [fullcontact/full.core "0.7.4-SNAPSHOT"]
+                 [fullcontact/full.time "0.7.4-SNAPSHOT"]
+                 [fullcontact/camelsnake "0.7.4-SNAPSHOT"]
+                 [fullcontact/full.json "0.7.4-SNAPSHOT"]
+                 [fullcontact/full.async "0.7.4-SNAPSHOT"]
+                 [fullcontact/full.dev "0.7.4-SNAPSHOT"]
+                 [fullcontact/full.cache "0.7.4-SNAPSHOT"]
+                 [fullcontact/full.metrics "0.7.4-SNAPSHOT"]
+                 [fullcontact/full.http "0.7.4-SNAPSHOT"]]
 
   :aot :all
 
