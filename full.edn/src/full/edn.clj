1cad279268a32d8aaeb8adcc65ec4a39ba230f19
Karlis Lauva <k@karlis.me>
2015-08-14 16:15:25 -0600

    add full.edn/read-edn-resource

--- full.edn/src/full/edn.clj
+++ full.edn/src/full/edn.clj
@@ -1,15 +1,24 @@
 (ns full.edn
   (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]
             [full.core.sugar :refer :all]
             [full.time :refer :all]))
 
- (defn read-edn
-   "Deserialize EDN string. Main difference from vanilla read-string is that
-   #inst timestamps are read as Joda DateTime not java.util.Date."
-   [string]
-   (let [opts {:readers (assoc *data-readers* 'inst read-instant-dt)}]
-     (edn/read-string opts string)))
 
-(defn write-edn
-  [object]
-  (pr-str object))
+(def ^:private edn-reader-opts
+  {:readers (assoc *data-readers* 'inst read-instant-dt)})
+
+(defn read-edn
+  "Deserialize EDN string. Main difference from vanilla read-string is that
+   #inst timestamps are read as Joda DateTime not java.util.Date."
+  [string]
+  (edn/read-string edn-reader-opts string))
+
+(defn read-edn-resource
+  "Load and deserialize and EDN resource file. #inst timestamps are read as
+  Joda DateTime instances. Returns nil if resource does not exist."
+  [resource-path]
+  (some->> (io/resource resource-path)
+           (io/reader)
+           (java.io.PushbackReader.)
+           (edn/read edn-reader-opts)))
