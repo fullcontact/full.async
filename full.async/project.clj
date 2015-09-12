511a9d9d4955793d16a68026a774c2bedfe9bf3a
zarlen <nelrahcaz@gmail.com>
2015-09-12 08:17:03 -0600

    Version 0.8.11-SNAPSHOT

--- full.async/project.clj
+++ full.async/project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.async "0.8.10"
+(defproject fullcontact/full.async "0.8.11-SNAPSHOT"
   :description "Extensions and helpers for core.async."
 
   :url "https://github.com/fullcontact/full.monty"
--- full.http/project.clj
+++ full.http/project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.http "0.8.10"
+(defproject fullcontact/full.http "0.8.11-SNAPSHOT"
   :description "Async HTTP client and server on top of http-kit and core.async."
 
   :url "https://github.com/fullcontact/full.monty"
@@ -14,11 +14,11 @@
                  [compojure "1.3.4" :exclusions [clj-time]]
                  [javax.servlet/servlet-api "2.5"]
                  [ring-cors "0.1.7"]
-                 [fullcontact/camelsnake "0.8.10"]
-                 [fullcontact/full.json "0.8.10"]
-                 [fullcontact/full.metrics "0.8.10"]
-                 [fullcontact/full.async "0.8.10"]
-                 [fullcontact/full.core "0.8.10"]]
+                 [fullcontact/camelsnake "0.8.11-SNAPSHOT"]
+                 [fullcontact/full.json "0.8.11-SNAPSHOT"]
+                 [fullcontact/full.metrics "0.8.11-SNAPSHOT"]
+                 [fullcontact/full.async "0.8.11-SNAPSHOT"]
+                 [fullcontact/full.core "0.8.11-SNAPSHOT"]]
 
   :aot :all
 
