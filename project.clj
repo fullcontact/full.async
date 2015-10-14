210251041b128a44213d7d10b5aea0b075fdf763
Kaspars Dancis <kaspars@fullcontact.com>
2015-10-14 15:34:41 -0600

    Version 0.8.14-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.monty "0.8.13"
+(defproject fullcontact/full.monty "0.8.14-SNAPSHOT"
   :description "Minimalistic stack for building robust Clojure HTTP services."
 
   :url "https://github.com/fullcontact/full.monty"
