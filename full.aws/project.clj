f5a46c62f55529c3d43273ba0e84bfcdd8298979
Kaspars Dancis <kaspars@fullcontact.com>
2015-10-14 15:31:17 -0600

    Version 0.8.13

--- full.aws/project.clj
+++ full.aws/project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.aws "0.8.13-SNAPSHOT"
+(defproject fullcontact/full.aws "0.8.13"
   :description "Async Amazon Webservices client."
 
   :url "https://github.com/fullcontact/full.monty"
@@ -14,13 +14,13 @@
                  [com.taoensso/faraday "1.7.1" ; DynamoDB sugar
                   :exclusions [com.amazonaws/aws-java-sdk-dynamodb joda-time]]
                  ; include full.time for joda-time dependency
-                 [fullcontact/full.time "0.8.13-SNAPSHOT"]
-                 [fullcontact/full.metrics "0.8.13-SNAPSHOT"]
-                 [fullcontact/full.http "0.8.13-SNAPSHOT"]
-                 [fullcontact/full.json "0.8.13-SNAPSHOT"]
-                 [fullcontact/full.edn "0.8.13-SNAPSHOT"]
-                 [fullcontact/full.async "0.8.13-SNAPSHOT"]
-                 [fullcontact/full.core "0.8.13-SNAPSHOT"]]
+                 [fullcontact/full.time "0.8.13"]
+                 [fullcontact/full.metrics "0.8.13"]
+                 [fullcontact/full.http "0.8.13"]
+                 [fullcontact/full.json "0.8.13"]
+                 [fullcontact/full.edn "0.8.13"]
+                 [fullcontact/full.async "0.8.13"]
+                 [fullcontact/full.core "0.8.13"]]
 
   :aot :all
 
