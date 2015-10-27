69f6a5cb0aab74bf425739631817862de11a3f74
zarlen <nelrahcaz@gmail.com>
2015-10-27 11:17:27 -0600

    Version 0.8.16-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.async "0.8.15"
+(defproject fullcontact/full.async "0.8.16-SNAPSHOT"
   :description "Extensions and helpers for core.async."
 
   :url "https://github.com/fullcontact/full.monty"
