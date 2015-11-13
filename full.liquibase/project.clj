7e9b366f4c3bf96a19b90643333cf033abb05377
Kaspars Dancis <kaspars@fullcontact.com>
2015-11-13 11:12:28 -0700

    Version 0.8.18

--- full.liquibase/project.clj
+++ full.liquibase/project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.liquibase "0.8.18-SNAPSHOT"
+(defproject fullcontact/full.liquibase "0.8.18"
   :description "Liquibase database schema creation/upgrading."
 
   :url "https://github.com/fullcontact/full.monty"
@@ -15,7 +15,7 @@
                  [com.mattbertolini/liquibase-slf4j "1.2.1"
                   :exclusions [org.slf4j/slf4j-api
                                org.yaml/snakeyaml]]
-                 [fullcontact/full.core "0.8.18-SNAPSHOT"]]
+                 [fullcontact/full.core "0.8.18"]]
 
   :aot :all
 
