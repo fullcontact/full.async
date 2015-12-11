14e09f41edb466b05cc99a74c6c8ae20a2e8f82b
Kaspars Dancis <kaspars@fullcontact.com>
2015-12-11 13:26:51 -0700

    0.8.20-SNAPSHOT

--- camelsnake/project.clj
+++ camelsnake/project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/camelsnake "0.8.19-SNAPSHOT"
+(defproject fullcontact/camelsnake "0.8.20-SNAPSHOT"
   :description "String and keyword transformation between cases."
 
   :url "https://github.com/fullcontact/full.monty"
