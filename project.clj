fccc1db641c85bcccf68eeb73e342c58edfb082b
Kaspars Dancis <kaspars@fullcontact.com>
2016-10-28 13:56:07 -0600

    bump to clojure 1.8 & latest core.async

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
   :deploy-repositories [["releases" {:url "https://clojars.org/repo/" :creds :gpg}]]
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/core.async "0.2.374"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.async "0.2.395"]]
   :aot :all
   :plugins [[lein-midje "3.1.3"]]
   :release-tasks [["vcs" "assert-committed"]
